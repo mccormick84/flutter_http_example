7e7a29fcffc76234f7cb9d708172ac9247f1a08e
Kenny <mccormick84@gmail.com>
2022-02-23 19:32:46 +0900

    검색 기능 추가하기

--- lib/main.dart
+++ lib/main.dart
@@ -25,18 +25,42 @@ class HttpApp extends StatefulWidget {
 class _HttpApp extends State<HttpApp> {
   String result = '';
   List? data;
+  TextEditingController? _editingController;
+  ScrollController? _scrollController;
+  int page = 1;
 
   @override
   void initState() {
     super.initState();
     data = new List.empty(growable: true);
+    _editingController = new TextEditingController();
+    _scrollController = new ScrollController();
+
+    // 스크롤 할 때마다 offset을 검사해 maxScrollExtent 보다 크거나 같고
+    // 스크롤 컨트롤러의 position에 정의된 범위를 넘어가지 않으면 목록의 마지막이라고 인식
+    _scrollController!.addListener(() {
+      if (_scrollController!.offset >=
+              _scrollController!.position.maxScrollExtent &&
+          !_scrollController!.position.outOfRange) {
+        debugPrint('bottom');
+        // 그러면 page를 1증가 시킨 후 getJSONData() 함수를 호출
+        page++;
+        getJSONData();
+      }
+    });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('HTTP Example'),
+        title: TextField(
+          controller: _editingController,
+          style: const TextStyle(color: Colors.white),
+          keyboardType: TextInputType.text,
+          decoration: const InputDecoration(
+              hintText: '검색어를 입력하세요'), // 텍스트필드 위젯에 보이는 텍스트를 꾸미는 옵션
+        ),
       ),
       body: Center(
         child: data!.isEmpty
@@ -58,7 +82,7 @@ class _HttpApp extends State<HttpApp> {
                         ),
                         Column(
                           children: <Widget>[
-                            Container(
+                            SizedBox(
                               width: MediaQuery.of(context).size.width - 150,
                               child: Text(
                                 data![index]['title'].toString(),
@@ -76,10 +100,13 @@ class _HttpApp extends State<HttpApp> {
                   );
                 },
                 itemCount: data!.length,
+                controller: _scrollController,
               ),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
+          page = 1;
+          data!.clear();
           getJSONData();
         },
         child: const Icon(Icons.file_download),
@@ -88,7 +115,8 @@ class _HttpApp extends State<HttpApp> {
   }
 
   Future<String> getJSONData() async {
-    var url = 'https://dapi.kakao.com/v3/search/book?target=title&query=doit';
+    var url = 'https://dapi.kakao.com/v3/search/book?'
+        'target=title&page=$page&query=${_editingController!.value.text}';
     var response = await http.get(Uri.parse(url),
         headers: {'Authorization': "KakaoAK 2f6ee3fb00ba02dcd4cfd694dd5dbdba"});
     setState(() {
